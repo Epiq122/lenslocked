e122425259fab5fe96f1ce8fe0ea000899098a0b
Robert Gleason <l2ob122@gmail.com>
2024-04-04 19:01:42 -0700

    Add faqHandler to handle FAQ page requests

--- main.go
+++ main.go
@@ -15,6 +15,27 @@ func contactHandler(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">robgleason122@gmail.com</a>.</p>")
 }
 
+func faqHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, ` <h1>🤔 Frequently Asked Questions 🤔</h1>
+	<ul>
+			<li>
+					<b>Is there a free version?</b>
+					<p>Yep! Our free version is so good that you'll wonder why anyone would ever need the paid one. Just kidding, the paid version has unicorns 🦄</p>
+			</li>
+			<li>
+					<b>What are your support hours?</b>
+					<p>Our support team is fueled by coffee ☕ and memes 😂, so they're available 24/7. But beware, if you ask for help during a full moon, you might get a werewolf instead. 🐺</p>
+			</li>
+			<li>
+					<b>How do I contact support?</b>
+					<p>Summon them by shouting "Supporto Patronum!" into a mirror three times. Or just email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a></p>
+			</li>
+	</ul>
+`)
+
+}
+
 type Router struct{}
 
 func (r Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
@@ -23,6 +44,8 @@ func (r Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		homeHandler(w, req)
 	case "/contact":
 		contactHandler(w, req)
+	case "/faq":
+		faqHandler(w, req)
 	default:
 		http.Error(w, "Page not found", http.StatusNotFound)
 	}
