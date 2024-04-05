16cb61ad85c7d16aa6807c2ae1c7bfc1313146e8
Robert Gleason <l2ob122@gmail.com>
2024-04-04 18:21:36 -0700

    Add contactHandler to handle contact page requests

--- main.go
+++ main.go
@@ -6,11 +6,18 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to the danger zones!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">robgleason122@gmail.com</a>.</p>")
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting server on port :3000...")
 	http.ListenAndServe(":3000", nil)
 }
