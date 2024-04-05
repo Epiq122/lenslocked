a671e99d651dd4c479e74b539f831fe3d6877474
Robert Gleason <l2ob122@gmail.com>
2024-04-04 18:29:55 -0700

    Refactor URL path handling in main.go created a small custom routing

--- main.go
+++ main.go
@@ -5,19 +5,29 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to the danger zones!</h1>")
 }
 
-func contactHandler(w http.ResponseWriter, r *http.Request) {
+func contactHandler(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">robgleason122@gmail.com</a>.</p>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		//TODO: handle page not found error
+	}
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting server on port :3000...")
 	http.ListenAndServe(":3000", nil)
 }
