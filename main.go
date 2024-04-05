f0a4ee174021a1465380470439eba6c08a8a5aaa
Robert Gleason <l2ob122@gmail.com>
2024-04-04 22:02:33 -0700

    Refactor main.go to use StaticHandler for home page rendering, add template rendering for contact and FAQ pages, and handle errors

--- main.go
+++ main.go
@@ -3,23 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/epiq122/lenslocked/controllers"
+	"github.com/epiq122/lenslocked/templates"
 	"github.com/epiq122/lenslocked/views"
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	r := chi.NewRouter()
-	tpl := views.Must(views.Parse(filepath.Join("templates", "home.tmpl")))
+	tpl := views.Must(views.ParseFS(templates.FS, "home.tmpl"))
 	r.Get("/", controllers.StaticHandler(tpl))
 
 	r.Get("/contact", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "contact.tmpl")))))
+		views.Must(views.ParseFS(templates.FS, "contact.tmpl"))))
 
 	r.Get("/faq", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "faq.tmpl")))))
+		views.Must(views.ParseFS(templates.FS, "faq.tmpl"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
