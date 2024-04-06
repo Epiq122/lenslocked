6bd3c70dfe0e0f0247ff34c91a09b24435460604
Robert Gleason <l2ob122@gmail.com>
2024-04-05 19:47:30 -0700

    Refactor main.go to use StaticHandler for home page rendering, add template rendering for contact and FAQ pages, and handle errors

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/epiq122/lenslocked/views"
@@ -18,5 +19,31 @@ func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
 	}
+}
 
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "Will there be a free version?",
+			Answer:   "Yes! we offer a free 30 day free trial",
+		},
+		{
+			Question: "What are your support hours?",
+			Answer:   "We have a support staff answering emails 24hrs a day",
+		},
+		{
+			Question: "What's your boogle?",
+			Answer:   "No boggle here!",
+		},
+		{
+			Question: "Do you have an office?",
+			Answer:   "Nope!!",
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
+	}
 }
